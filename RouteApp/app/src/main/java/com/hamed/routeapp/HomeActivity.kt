5e4b0cf6d25e9a404586f493857f55de44265783
Ahmed Hamed <ahmedhamed7728@gmail.com>
2024-07-11 02:20:18 +0300

    splash screen

--- RouteApp/app/src/main/java/com/hamed/routeapp/HomeActivity.kt
+++ RouteApp/app/src/main/java/com/hamed/routeapp/HomeActivity.kt
@@ -1,38 +1,20 @@
 package com.hamed.routeapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-// Splash screen
-class MainActivity : AppCompatActivity() {
-
-    private val DELAY: Long = 1000
-
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
+        setContentView(R.layout.home_acitivity)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            startHomeActivity()
-        }, DELAY)
-    }
-
-    private fun startHomeActivity() {
-
-        val navigationIntent = Intent(this, HomeActivity::class.java);
-        startActivity(navigationIntent)
     }
 }
-
-
